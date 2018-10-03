78cb560aee3a6982e13f27ac3b8ec669acc7135d
jzwart <jzwart@usgs.gov>
2018-10-03 18:11:15 -0500

    updating with doc

--- R/corr_btw_forecasts.R
+++ R/corr_btw_forecasts.R
@@ -23,6 +23,7 @@ out # DIC / CO2 / DOC model
 Y1 = out1$Y
 Y = out$Y
 
+# co2
 co2_doc_mod = apply(Y1[6,1,,],MARGIN = 1,FUN=mean)/data2$epiVol*12
 
 dic_co2_doc_mod = apply(Y[7,1,,],MARGIN = 1,FUN=mean)/data2$epiVol*12
@@ -30,6 +31,7 @@ dic_co2_doc_mod = apply(Y[7,1,,],MARGIN = 1,FUN=mean)/data2$epiVol*12
 
 cor(co2_doc_mod, dic_co2_doc_mod)
 sqrt(mean((co2_doc_mod - dic_co2_doc_mod)^2))
+
 
 ylim = c(0.2,0.7)
 xlim = ylim
@@ -52,7 +54,22 @@ abline(0,1, lwd=2, lty=2, col ='red')
 
 # AIC(logLik(lm(co2_doc_mod~dic_co2_doc_mod)))
 
+# doc
+co2_doc_mod = apply(Y1[9,1,,],MARGIN = 1,FUN=mean)/data2$epiVol*12
 
+dic_co2_doc_mod = apply(Y[10,1,,],MARGIN = 1,FUN=mean)/data2$epiVol*12
+
+
+cor(co2_doc_mod, dic_co2_doc_mod)
+sqrt(mean((co2_doc_mod - dic_co2_doc_mod)^2))
+
+ylim = c(9,11)
+xlim = ylim
+plot(co2_doc_mod~ dic_co2_doc_mod, xlim = xlim, ylim =ylim, pch=16, ylab= 'CO2 Only Model', xlab='Carbonate Model')
+abline(0,1, lty = 2, lwd = 2)
+
+
+#co2
 windows()
 l_mar = 0.35
 b_mar = 0.1
@@ -99,10 +116,66 @@ plot(z$y[2,1,assim_obs==1,1]/data2$epiVol[assim_obs==1]*12~as.POSIXct(data2$date
 arrows(as.POSIXct(data2$datetime[assim_obs==1]),z$y[2,1,assim_obs==1,1]/data2$epiVol[assim_obs==1]*12-co2PoolSD[assim_obs==1]/data2$epiVol[assim_obs==1]*12,
        as.POSIXct(data2$datetime[assim_obs==1]),z$y[2,1,assim_obs==1,1]/data2$epiVol[assim_obs==1]*12+co2PoolSD[assim_obs==1]/data2$epiVol[assim_obs==1]*12,
        code=3,length=0.1,angle=90,col='black',lwd=3)
-# legend("topleft", legend=c("Estimated State","Ensemble Mean",'Observed State'),
-#        col=c('gray','gray30','black'),pt.bg=c('gray','gray30','black'),cex = cex.axis,
-#        ncol=1,lwd=c(4,4,0),bty='n',lty=c(1,1,0),pt.cex = c(0,0,2),pch = c(0,0,16))
-# text(x=xlim[2]-leg*(xlim[2]-xlim[1]),y = ylim[1]+leg*(ylim[2]-ylim[1]),labels = 'B', cex = cex.lab)
+
+
+# doC
+windows()
+l_mar = 0.35
+b_mar = 0.1
+t_mar = 0.05
+r_mar= 0.05
+gapper = 0.15 # space between panels
+leg = 0.05 # distance from corner for panel label
+
+cex=4
+cex.lab=2
+cex.axis=2
+lwd=3
+ylim=c(4,12)
+xlim=range(as.POSIXct(data2$datetime))
+par(mar = c(5,6,1,1))
+DOCout<-apply(Y[10,1,,]/data2$epiVol*12,MARGIN = 1,FUN=mean)
+ylim=range(c(Y[10,1,,]/data2$epiVol*12,z$y[3,1,,1]/data2$epiVol*12+docPoolSD/data2$epiVol*12,z$y[3,1,,1]/data2$epiVol*12-docPoolSD/data2$epiVol*12),na.rm=T)
+plot(DOCout~as.POSIXct(data2$datetime),type='l',ylim=ylim,lwd=lwd,cex.axis=cex.axis, col= t_col('red',80),
+     ylab=expression(DOC~(mg~C~L^-1)),xlab='',cex.lab=cex.lab)
+for(i in 1:nEn){
+  lines(Y[10,1,,i]/data2$epiVol*12~as.POSIXct(data2$datetime),
+        col=t_col('red', 80),ylab='',lwd=lwd)
+}
+lines(DOCout~as.POSIXct(data2$datetime),ylab='',lwd=3,col=t_col('red',80))
+par(new=T)
+DOCout<-apply(Y1[9,1,,]/data2$epiVol*12,MARGIN = 1,FUN=mean)
+plot(DOCout~as.POSIXct(data2$datetime),type='l',ylim=ylim,lwd=lwd,cex.axis=cex.axis,col=t_col('blue',80),
+     ylab=expression(DOC~(mg~C~L^-1)),xlab='',cex.lab=cex.lab)
+for(i in 1:nEn){
+  lines(Y1[9,1,,i]/data2$epiVol*12~as.POSIXct(data2$datetime),
+        col=t_col('light blue',80),ylab='',lwd=lwd)
+}
+lines(DOCout~as.POSIXct(data2$datetime),ylab='',lwd=3,col=t_col('blue',80))
+
+par(new=T)
+plot(z$y[3,1,assim_obs==0,1]/data2$epiVol[assim_obs==0]*12~as.POSIXct(data2$datetime[assim_obs==0]),cex=cex,
+     ylim=ylim,col='black',pch=21,ylab = '',xlab='',cex.lab=cex.lab,cex.axis=cex.axis,xlim=xlim)
+arrows(as.POSIXct(data2$datetime[assim_obs==0]),z$y[3,1,assim_obs==0,1]/data2$epiVol[assim_obs==0]*12-docPoolSD[assim_obs==0]/data2$epiVol[assim_obs==0]*12,
+       as.POSIXct(data2$datetime[assim_obs==0]),z$y[3,1,assim_obs==0,1]/data2$epiVol[assim_obs==0]*12+docPoolSD[assim_obs==0]/data2$epiVol[assim_obs==0]*12,
+       code=3,length=0.1,angle=90,col='black',lwd=3)
+par(new=T)
+plot(z$y[3,1,assim_obs==1,1]/data2$epiVol[assim_obs==1]*12~as.POSIXct(data2$datetime[assim_obs==1]),cex=cex,
+     ylim=ylim,col='black',pch=16,ylab='',xlab='',cex.lab=cex.lab,cex.axis=cex.axis,xlim=xlim)
+arrows(as.POSIXct(data2$datetime[assim_obs==1]),z$y[3,1,assim_obs==1,1]/data2$epiVol[assim_obs==1]*12-docPoolSD[assim_obs==1]/data2$epiVol[assim_obs==1]*12,
+       as.POSIXct(data2$datetime[assim_obs==1]),z$y[3,1,assim_obs==1,1]/data2$epiVol[assim_obs==1]*12+docPoolSD[assim_obs==1]/data2$epiVol[assim_obs==1]*12,
+       code=3,length=0.1,angle=90,col='black',lwd=3)
+
+
+
+
+
+
+
+
+
+
+
 
 
 # diff in pH
